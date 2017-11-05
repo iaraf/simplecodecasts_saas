be1a92df963b93ef501f8bb5a21e43a54a0dceff
Farai Savage <farai_savage@hotmail.com>
2017-11-05 17:10:41 +0000

    added notice and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     def configure_permitted_perameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
     end
+    
+    private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
